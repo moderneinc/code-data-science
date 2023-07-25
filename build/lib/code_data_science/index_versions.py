f77b2de5095c767acb994f1cf80f2441d5ee98f9
Kyle Scully <scullykns@gmail.com>
2023-07-24 18:55:12 -0700

    chore: remove/ignore build directory

--- build/lib/code_data_science/index_versions.py
+++
@@ -1,6 +0,0 @@
-from code_data_science.sort_versions import sort_versions
-
-def index_versions(versions, metadata_pattern=None):
-    sorted_versions = sort_versions(set(versions), metadata_pattern)
-    return { v : sorted_versions.index(v) for v in sorted_versions }
-
