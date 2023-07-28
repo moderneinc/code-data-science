d36686a151dddedb69e462ce7c0e7b5431f39a3b
Jonathan Schneider <jkschneider@gmail.com>
2023-07-28 12:07:31 -0400

    Add clustering and starencoder

---
+++ code_data_science/clustering_src/constants.py
@@ -0,0 +1,16 @@
+# Various constants.
+
+PADDING_ID_FOR_LABELS = -100  # used to convert padding ids to ignored labels.
+MAX_VALID_DATA_ROW_COUNT = 2000
+
+# Data path:
+GFG_DATA_PATH = "./resources/data/transcoder_evaluation_gfg"
+
+# Special tokens
+MASK_TOKEN = "<mask>"
+SEPARATOR_TOKEN = "<sep>"
+PAD_TOKEN = "<pad>"
+CLS_TOKEN = "<cls>"
+
+# Results notebook name.
+RESULTS_FNAME = "results.ipynb"
