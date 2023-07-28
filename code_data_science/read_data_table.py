d5ee8a499d85b19ab5026e4a450a64c576f64e9c
Jonathan Schneider <jkschneider@gmail.com>
2023-07-28 15:39:44 -0400

    Rename read_data_table

--- code_data_science/read_data_table.py
+++ code_data_science/read_data_table.py
@@ -8,4 +8,4 @@ FilePath = Union[str, "PathLike[str]"]
 
 
 def read_data_table(sample: FilePath) -> DataFrame:
-    return pd.read_csv(os.environ.get('NB_DATA_TABLE', sample))
+    return pd.read_csv(os.environ.get('NB_DATA_TABLE', sample), on_bad_lines='skip', skip_blank_lines=True)
