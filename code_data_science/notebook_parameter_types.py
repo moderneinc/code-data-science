ab0bb2dc7dad32ff3857739905e88833a64837d3
Kyle Scully <scullykns@gmail.com>
2023-08-14 22:29:27 -0700

    feat: add notebook parameter type aliases

---
+++ code_data_science/notebook_parameter_types.py
@@ -0,0 +1,8 @@
+"""
+This file contains type aliases to use for notebook parameters (via papermill).
+Papermill allows parameters to be any type but these type aliases are intended
+to be used to make the experience of running visualizations consistent.
+"""
+String = str
+Boolean = bool
+StringList = list[str]
